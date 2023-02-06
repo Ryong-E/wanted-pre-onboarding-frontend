5399a00ea88d37a0ad16bbb7a5769620e4532ea7
Ryong-E <think8867@gmail.com>
2023-02-06 18:00:45 +0900

    style: style 초기화, theme provider 설정

---
+++ src/components/buttons/LoginButton.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function LoginButton() {
+  return <div>LoginButton</div>;
+}
+
+const Button = styled.button``;
+export default LoginButton;
---
+++ src/styles/theme.js
@@ -0,0 +1,7 @@
+export const theme = {
+  colors: {
+    error: '#ED5C48',
+    white: '#FFFFFF',
+    placeholder: '#A9A9A9',
+  },
+};
