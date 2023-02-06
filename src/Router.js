ef73544a7020abd5c9207b9c0f8aaeeb9ba3adb6
Ryong-E <think8867@gmail.com>
2023-02-06 20:37:08 +0900

    feat(Router): header 추가

--- src/Router.js
+++ src/Router.js
@@ -1,18 +1,19 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
 import { ROUTE_ARR } from './constant/route';
 import Auth from './hoc/auth';
 
 function Router() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
-        {ROUTE_ARR.map((route) => {
+        {ROUTE_ARR.map((route, index) => {
           const AuthCheckComp = Auth(route.element, route.authenticated);
-          return <Route path={route.path} element={<AuthCheckComp />} />;
+          return <Route path={route.path} element={<AuthCheckComp />} key={index} />;
         })}
       </Routes>
     </BrowserRouter>
   );
 }
-
 export default Router;
