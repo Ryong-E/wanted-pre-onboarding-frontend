f7c6eac08f2837446c8811bcd00e19d2f876c920
Ryong-E <think8867@gmail.com>
2023-02-08 16:15:38 +0900

    refactor(LargeButton): props 추가, style변경

--- src/components/buttons/LargeButton.jsx
+++ src/components/buttons/LargeButton.jsx
@@ -1,21 +1,25 @@
 import React from 'react';
 import styled from 'styled-components';
 
-function LargeButton({ color, children, onClick }) {
+function LargeButton({ color, children, onClick, mode, disabled }) {
   return (
-    <Button color={color} onClick={onClick}>
+    <Button color={color} onClick={onClick} disabled={disabled}>
       {children}
     </Button>
   );
 }
 
 const Button = styled.button`
-  width: 100vw;
+  width: 100%;
   max-width: 400px;
   height: 50px;
   background-color: ${(props) => props.color};
   border-radius: 12px;
   color: white;
+  &:disabled {
+    background-color: rgb(217, 217, 217);
+    cursor: not-allowed;
+  }
 `;
 
 export default LargeButton;
